840f7871a789d59325b2bc55ade50d00885a6c5a
Franco Ponticelli <franco.ponticelli@gmail.com>
2014-10-19 11:57:26 -0600

    more extractions

--- gen/Generate.hx
+++ gen/Generate.hx
@@ -1,3 +1,5 @@
+import cs.Lib;
+
 import cs.system.globalization.*;
 
 using thx.core.Arrays;
@@ -7,52 +9,139 @@ using StringTools;
 class Generate {
   static var pattern = ~/^([^(]+)\(([^)]+)\)$/;
   public static function main() {
-    var all = cs.Lib.array(CultureInfo.GetCultures(CultureTypes.AllCultures)),
+    var all = Lib.array(CultureInfo.GetCultures(CultureTypes.AllCultures)),
         acc = [],
-        nameNative, nameEnglish, regionNative, regionEnglish;
+        nativeName, englishName, nativeRegion, englishRegion;
 
     for(ci in all) {
       if(pattern.match(ci.NativeName)) {
-        nameNative = StringTools.trim(pattern.matched(1));
-        regionNative = StringTools.trim(pattern.matched(2));
+        nativeName = StringTools.trim(pattern.matched(1));
+        nativeRegion = StringTools.trim(pattern.matched(2));
 
         pattern.match(ci.EnglishName);
-        nameEnglish = StringTools.trim(pattern.matched(1));
-        regionEnglish = StringTools.trim(pattern.matched(2));
+        englishName = StringTools.trim(pattern.matched(1));
+        englishRegion = StringTools.trim(pattern.matched(2));
       } else {
-        nameNative = ci.NativeName;
-        nameEnglish = ci.EnglishName;
-        regionNative = null;
-        regionEnglish = null;
+        nativeName = ci.NativeName;
+        englishName = ci.EnglishName;
+        nativeRegion = null;
+        englishRegion = null;
       }
-      acc.push({
-        nameNative    : nameNative,
-        nameEnglish   : nameEnglish,
-        regionNative  : regionNative,
-        regionEnglish : regionEnglish,
-        code          : ci.Name,
-        ietf          : ci.IetfLanguageTag,
-        lcid          : ci.LCID,
-        isNeutral     : ci.IsNeutralCulture,
-        iso2          : ci.TwoLetterISOLanguageName,
-        iso3          : ci.ThreeLetterISOLanguageName,
-        windows3      : ci.ThreeLetterWindowsLanguageName,
+      var cal = ci.Calendar,
+          dateTimeFormat = null,
+          numberFormat = null;
 
 /*
-        //ci.Calendar;
-        //ci.CompareInfo;
-        //ci.DateTimeFormat;
+        //ci.CompareInfo; ???
         //ci.NumberFormat;
         //ci.OptionalCalendars;
         //ci.Parent;
         //ci.TextInfo;
 */
+
+      if(!ci.IsNeutralCulture) {
+        // DATETIME
+        var dt = ci.DateTimeFormat;
+        dateTimeFormat = {
+          designators : {
+            am : dt.AMDesignator,
+            pm : dt.PMDesignator
+          },
+          names : {
+            days              : Lib.array(dt.DayNames),
+            abbreviatedDays   : Lib.array(dt.AbbreviatedDayNames),
+            shortestDays      : Lib.array(dt.ShortestDayNames),
+            abbreviatedMonthGenitives : Lib.array(dt.AbbreviatedMonthGenitiveNames),
+            monthGenitives    : Lib.array(dt.MonthGenitiveNames),
+            months            : Lib.array(dt.MonthNames),
+            abbreviatedMonths : Lib.array(dt.AbbreviatedMonthNames),
+          },
+          patterns : {
+            fullDateTime      : dt.FullDateTimePattern,
+            longDate          : dt.LongDatePattern,
+            longTime          : dt.LongTimePattern,
+            monthDay          : dt.MonthDayPattern,
+            rfc1123           : dt.RFC1123Pattern,
+            shortDate         : dt.ShortDatePattern,
+            shortTime         : dt.ShortTimePattern,
+            sortableDateTime  : dt.SortableDateTimePattern,
+            universalSortable : dt.UniversalSortableDateTimePattern,
+            yearMonth         : dt.YearMonthPattern,
+          },
+          calendarName        : ('' + dt.Calendar).split('.').pop().substr(0, -8),
+          nativeCalendarName  : try dt.NativeCalendarName catch(e : Dynamic) null,
+          calendarWeekRule    : dt.CalendarWeekRule,
+          dateSeparator       : dt.DateSeparator,
+          firstDayOfWeek      : dt.FirstDayOfWeek,
+          timeSeparator       : dt.TimeSeparator,
+        };
+
+        // NUMBER FORMAT
+        var nf = ci.NumberFormat;
+        
+/*
+*/
+        numberFormat = {
+          currency : {
+            decimalDigits    : nf.CurrencyDecimalDigits,
+            decimalSeparator : nf.CurrencyDecimalSeparator,
+            groupSeparator   : nf.CurrencyGroupSeparator,
+            groupSizes       : Lib.array(nf.CurrencyGroupSizes),
+            negativePattern  : nf.CurrencyNegativePattern,
+            positivePattern  : nf.CurrencyPositivePattern,
+            symbol           : nf.CurrencySymbol,
+          },
+          number : {
+            decimalDigits    : nf.NumberDecimalDigits,
+            decimalSeparator : nf.NumberDecimalSeparator,
+            groupSeparator   : nf.NumberGroupSeparator,
+            groupSizes       : Lib.array(nf.NumberGroupSizes),
+            negativePattern  : nf.NumberNegativePattern,
+          },
+          percent : {
+            perMilleSymbol   : nf.PerMilleSymbol,
+            decimalDigits    : nf.PercentDecimalDigits,
+            decimalSeparator : nf.PercentDecimalSeparator,
+            groupSeparator   : nf.PercentGroupSeparator,
+            groupSizes       : nf.PercentGroupSizes,
+            negativePattern  : nf.PercentNegativePattern,
+            positivePattern  : nf.PercentPositivePattern,
+          },
+          //digitSubstitution  : nf.DigitSubstitution.getName(),
+          naNSymbol              : nf.NaNSymbol,
+          //nativeDigits       : Lib.array(nf.NativeDigits),
+          negativeInfinitySymbol : nf.NegativeInfinitySymbol,
+          negativeSign           : nf.NegativeSign,
+          positiveInfinitySymbol : nf.PositiveInfinitySymbol,
+          positiveSign           : nf.PositiveSign,
+        };
+      }
+
+      acc.push({
+        nativeName     : nativeName,
+/*
+        englishName    : englishName,
+        nativeRegion   : nativeRegion,
+        englishRegion  : englishRegion,
+        code           : ci.Name,
+        ietf           : ci.IetfLanguageTag,
+        lcid           : ci.LCID,
+        isNeutral      : ci.IsNeutralCulture,
+        iso2           : ci.TwoLetterISOLanguageName,
+        iso3           : ci.ThreeLetterISOLanguageName,
+        windows3       : ci.ThreeLetterWindowsLanguageName,
+        calendarName   : ('' + cal).split('.').pop().substr(0, -8),
+        dateTimeFormat : dateTimeFormat,
+*/
+        numberFormat   : numberFormat,
+
+
       });
     }
 
     acc
-      .slice(90, 105)
-      //.order(function(a, b) return Strings.compare(a.nameNative, b.nameNative))
+      //.slice(90, 105)
+      //.order(function(a, b) return Strings.compare(a.nativeName, b.nativeName))
       .map(function(item) {
         trace(item);
       });
