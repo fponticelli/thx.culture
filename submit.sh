31e484bc00136ddb22c6bf03d87eaffdac853f52
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-03-31 20:13:10 -0600

    exclude hidden files from zip

--- submit.sh
+++ submit.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 rm thx.culture.zip
-zip -r thx.culture.zip hxml src test doc/ImportCulture.hx extraParams.hxml haxelib.json README.md test test.hxml
+zip -r thx.culture.zip hxml src test doc/ImportCulture.hx extraParams.hxml haxelib.json README.md test test.hxml -x "*/\.*"
 haxelib submit thx.culture.zip
