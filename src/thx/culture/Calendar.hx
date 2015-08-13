75e05659c22f1e21d254ffa64113f8e5c17b783b
Franco Ponticelli <franco.ponticelli@gmail.com>
2015-08-12 23:13:12 -0600

    more basics for Calendar

--- src/thx/culture/Calendar.hx
+++ src/thx/culture/Calendar.hx
@@ -1,9 +1,24 @@
 package thx.culture;
 
 class Calendar {
-  public var currentEraValue(default, null) : Int;
+  public var currentEra
+  public var eras(default, null) : Iterable<Int>;
 
-  public function new(currentEraValue : Int) {
-    this.currentEraValue = currentEraValue;
+  function new() {
+
   }
+
+  public function getEra(date : DateTime) : Int
+    return throw new thx.error.AbstractMethod();
+  public function getMonth(date : DateTime) : Int
+    return throw new thx.error.AbstractMethod();
+/**
+Era defaults to `currentEra` if not provided.
+*/
+  public function getMonthsInYear(year : Int, ?era : Int) : Int
+    return throw new thx.error.AbstractMethod();
+  public function getYear(date : DateTime) : Int
+    return throw new thx.error.AbstractMethod();
+  public function toDateTime(year : Int, month : Int, day : Int, hour : Int, minute : Int, second : Int, millisecond : Int, era : Int) : DateTime
+    return throw new thx.error.AbstractMethod();
 }
